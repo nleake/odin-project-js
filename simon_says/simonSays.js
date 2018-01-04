d62d250d3667d7aa28b72827f8d183d277708774
Nick Leake <leakend@dukes.jmu.edu>
2018-01-03 21:23:58 -0500

    add simon

--- simon_says/simonSays.js
+++ simon_says/simonSays.js
@@ -1,21 +1,34 @@
-function echo() {
-	
+function echo(input) {
+	return input;
 }
 
-function shout() {
-	
+function shout(input) {
+	return String(input).toUpperCase();
 }
 
-function repeat() {
-
+function repeat(string, num) {
+	let nStr = "";
+	if (!num) {
+		nStr += `${string} ${string}`;
+	}
+	let i = 0;
+	while (i < num) {
+		nStr += `${string} `;
+		i++;
+	}
+	return nStr.trim();
 }
 
-function pieceOfWord() {
-	
+function pieceOfWord(str, num) {
+	let nStr ="";
+	for (let i = 0; i < num; i++){
+		nStr += str[i];
+	}
+	return nStr;
 }
 
-function firstWord() {
-	
+function firstWord(str) {
+	return str.split(' ')[0];
 }
 
 function capitalize(word) {
@@ -23,8 +36,20 @@ function capitalize(word) {
 	// This function just capitalizes the word given to it, use in conjunction with titleCreator
 }
 
-function titleCreator() {
-	
+function titleCreator(str) {
+	let words = str.split(' ');
+	const littleWords = ['and', 'or', 'to', 'over', 'the'];
+	for (let i = 1; i < words.length; i++){
+		for(let j = 1; j < littleWords.length; j++){
+			if (words[i].toLowerCase() === littleWords[j]){
+				words[i] = words[i].toLowerCase();
+			} else {
+				words[i] = capitalize(words[i]);
+			}
+		}
+	}
+	capitalize(words[0]);
+	return words.join(" ");
 }
 
 module.exports = {
